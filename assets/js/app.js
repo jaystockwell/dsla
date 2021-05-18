f5af21eb112271fed695c1f4a89109d8fda8a077
Soichi Hayashi <soichih@gmail.com>
2021-05-18 00:51:39 -0400

    removing loged in user's name from header (too wide)

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,8 +52,7 @@ document.initLogin = (el)=> {
             <div v-if="!$root.user" class="btn btn-primary" @click="$root.login">Login</div>
             <div v-if="$root.user">
                 <div class="btn btn-outline-secondary" @click="$root.logout">Logout</div>
-                &nbsp;
-                <small>{{$root.user.name}}</small>
+                <!--&nbsp; <small>{{$root.user.name}}</small>-->
             </div>
         </div>`,
         });
